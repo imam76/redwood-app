6e4bea2ca2dbe6cc928dc5311334c1500f28978b
imam76 <asepimam335@gmail.com>
2024-11-04 09:25:04 +0700

    add navbar

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
+
+const items = new Array(5).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
 const Navbar = () => {
+
   return (
     <div>
-      <h2>{'Navbar'}</h2>
-      <p>{'Find me in ./web/src/components/Navbar/Navbar.tsx'}</p>
+      <Layout>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+      </Layout>
     </div>
-  )
+  );
+
 }
 
 export default Navbar
--- web/src/layouts/NavbarLayout/NavbarLayout.tsx
+++ web/src/layouts/NavbarLayout/NavbarLayout.tsx
@@ -1,4 +1,7 @@
 import Navbar from "src/components/Navbar/Navbar"
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 type NavbarLayoutProps = {
   children?: React.ReactNode
